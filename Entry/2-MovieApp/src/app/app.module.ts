898c23d95d8df1c1ea09df945686eeb7e56ea582
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-08-26 08:51:15 +0300

    More Component Added

--- Entry/2-MovieApp/src/app/app.module.ts
+++ Entry/2-MovieApp/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MoviesComponent } from "./movies/movies.component";
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule
