8dcfa0c68ef6e3e8bb01135f0aac63e5283c0b25
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-10-31 08:51:14 +0300

    Models Created
    Models Created

---
+++ AngularProjects/Northwind/src/app/Models/product.ts
@@ -0,0 +1,7 @@
+export interface Product{
+  productId: number;
+  productName: string;
+  categoryId: number;
+  unitInStock: number;
+  unitPrice: number;
+}
--- AngularProjects/Northwind/src/app/components/product/product.component.ts
+++ AngularProjects/Northwind/src/app/components/product/product.component.ts
@@ -1,53 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/Models/product';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
 
-  product1: any = {
-    productId: 1,
-    productName: 'Elmalı Kek 1',
-    categoryId: 1,
-    unitPrice: 6,
-  };
-  product2: any = {
-    productId: 2,
-    productName: 'Elmalı Kek 2',
-    categoryId: 2,
-    unitPrice: 4,
-  };
-  product3: any = {
-    productId: 3,
-    productName: 'Elmalı Kek 3',
-    categoryId: 3,
-    unitPrice: 5,
-  };
-  product4: any = {
-    productId: 4,
-    productName: 'Elmalı Kek 4',
-    categoryId: 2,
-    unitPrice: 7,
-  };
-  product5: any = {
-    productId: 5,
-    productName: 'Elmalı Kek 5',
-    categoryId: 1,
-    unitPrice: 6,
-  };
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
+  products: Product[] = [
+
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
