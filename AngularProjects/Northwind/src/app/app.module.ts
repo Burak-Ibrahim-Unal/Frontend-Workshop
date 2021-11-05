43f2ea2c1bb08d4c6fa3a1077bc8124333d05f20
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-11-05 09:58:38 +0300

    ReactiveFormsModule implementation
    ReactiveFormsModule implementation

--- AngularProjects/Northwind/src/app/app.module.ts
+++ AngularProjects/Northwind/src/app/app.module.ts
@@ -7,32 +7,35 @@ import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductComponent,
-    CategoryComponent,
-    NaviComponent,
-    VatAddedPipe,
-    FilterPipePipe,
-    CartSummaryComponent,
-  ],
-  imports: [BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot({
-      positionClass: "toast-bottom-right"
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        ProductComponent,
+        CategoryComponent,
+        NaviComponent,
+        VatAddedPipe,
+        FilterPipePipe,
+        CartSummaryComponent,
+        ProductAddComponent,
+    ],
+    imports: [BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        ToastrModule.forRoot({
+            positionClass: "toast-bottom-right"
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
