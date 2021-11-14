39757b4c86a8635118fc424713545a2164217840
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-11-14 19:47:37 +0300

    static components are ready... All will be dynamic later

--- AngularProjects/MovieApp/src/app/app.module.ts
+++ AngularProjects/MovieApp/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryComponent,
+    MoviesComponent,
+    MovieComponent,
+    MovieDetailsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
