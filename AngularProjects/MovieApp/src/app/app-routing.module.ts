545aee861b78f3085a6ea95f82c7b06afd776d3e
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-11-16 17:07:37 +0300

    routes module added.// route bug fixed

--- AngularProjects/MovieApp/src/app/app-routing.module.ts
+++ AngularProjects/MovieApp/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
+import { MoviesComponent } from './movies/movies.component';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+const routes: Routes = [
+  { path: "movies", component: MoviesComponent },
+  { path: "", redirectTo: "movies", pathMatch: "full" },
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- AngularProjects/MovieApp/src/app/app.module.ts
+++ AngularProjects/MovieApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DescSummaryPipe } from './Pipes/desc-summary.pipe';
 import { MovieFilterPipe } from './Pipes/movie-filter.pipe';
 import { AlertifyService } from './services/alertify.service';
 import { AppRoutingModule } from './app-routing.module';
+// import { RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // RouterModule
   ],
   providers: [
     AlertifyService
