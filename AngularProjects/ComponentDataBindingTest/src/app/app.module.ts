4e1fb8c0cebba659011bb801574234da522a912c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-11-20 22:19:42 +0300

    modules are splitted

--- AngularProjects/ComponentDataBindingTest/src/app/app.module.ts
+++ AngularProjects/ComponentDataBindingTest/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { HostComponent } from './host/host.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    HostComponent
   ],
   imports: [
     BrowserModule,
---
+++ AngularProjects/ComponentDataBindingTest/src/app/host/host.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-host',
+  templateUrl: './host.component.html',
+  styleUrls: ['./host.component.css']
+})
+export class HostComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+}
