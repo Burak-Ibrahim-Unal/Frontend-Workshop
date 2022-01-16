8dbc59f0257d90205e86d9a724df41bcee257d0a
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-16 12:25:49 +0300

    React Tests Started

---
+++ ReactProjects/Entry/TestApp1/index.js
@@ -0,0 +1,22 @@
+// ReactDOM.render(<p>Paragraph from js file</p>,document.getElementById("root")); // document.querySelector("#root")
+// ReactDOM.render(<ul><li>li 1</li><li>li 2</li></ul>,document.getElementById("root"));
+ReactDOM.render(<h1 className="Test">Paragraph from js file</h1>,document.getElementById("root")); // document.querySelector("#root")
+
+
+// function MainContent() {
+//   return (
+//     <h1>main content is here</h1>
+//   );
+// }
+
+// ReactDOM.render(
+//   <div>
+//     <MainContent />
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// const h1 = document.createElement("h1");
+// h1.textContent="main content h1 tag";
+// h1.className="header";
+// document.getElementById("root").append(h1);
