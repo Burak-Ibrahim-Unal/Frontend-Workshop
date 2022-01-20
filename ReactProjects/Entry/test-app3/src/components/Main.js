e0dd06aa2de3ccdd46e3fe3234516ee93bd06ac2
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-20 10:12:45 +0300

    React test app 3 main content and css are made

--- ReactProjects/Entry/test-app3/src/components/Main.js
+++ ReactProjects/Entry/test-app3/src/components/Main.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "../index.css";
+import logo from "../images/react-logo.png";
 
 export default function Main() {
     return (
-        <h1 className="fontInter400">h1 tag...</h1>
+        <main className="fontInter400">
+            <h1 className="mainH1"> All options listed above</h1>
+            <ul className="ulMain fontInter600">
+                <li>option 1</li>
+                <li>option 2</li>
+                <li>option 3</li>
+                <li>option 4</li>
+                <li>option 5</li>
+            </ul>
+        </main>
+
     );
 }
