883ee718f0a086d0926e6716a859c7a27e1609ba
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-21 16:12:43 +0300

    Uncessary test projects removed // testapp4 updated with props

--- ReactProjects/Entry/test-app4/src/App.js
+++ ReactProjects/Entry/test-app4/src/App.js
@@ -2,13 +2,22 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import boy1 from "./images/boy1.jpg";
+
 
 export default function App() {
     return (
         <div>
             <Navbar />
             {/* <Hero /> */}
-            <Card />
+            <Card 
+                img={boy1}
+                rating="4.9"
+                reviewCount={6}
+                country="Turkey" 
+                title="Little Boys at Arena"
+                price={40}
+            />
         </div>
     );
 }
--- ReactProjects/Entry/test-app4/src/components/Card.js
+++ ReactProjects/Entry/test-app4/src/components/Card.js
@@ -3,17 +3,18 @@ import boy1 from "../images/boy1.jpg";
 import star1 from "../images/star1.jpg";
 
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props.img);
     return (
         <div className="card">
-            <img className="cardImage" src={boy1} alt="boy1" />
+            <img className="cardImage" src={props.img} alt="boy1" />
             <div className="cardStats">
                 <img className="card-star" src={star1} alt="star" />
-                <span>9.8</span>
-                <span className="grayText">(12)• </span>
-                <span className="grayText"> &nbsp; Turkey</span>
+                <span>{props.rating}</span>
+                <span className="grayText">({props.reviewCount})• </span>
+                <span className="grayText"> &nbsp; {props.country}</span>
             </div>
-            <p><span className="bold">Lorem</span>,  ipsum dolor sit amet consectetur adipisicing.</p>
+            <p><span className="bold">{props.price} $</span>,  ipsum dolor sit amet consectetur adipisicing.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, tempora? Temporibus velit quae inventore atque! Architecto, ut sequi et quo ab itaque ea doloribus est.</p>
         </div>
     )
