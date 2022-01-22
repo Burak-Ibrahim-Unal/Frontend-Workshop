b2e9c343eced63f05842f57e7a758eb832995a19
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-22 13:43:14 +0300

    new testapp5 is started

---
+++ ReactProjects/Entry/test-app5/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Joke from "./Joke";
+import JokeData from "./JokeData";
+
+export default function App() {
+    console.log(JokeData);
+    return (
+        <div>
+
+        </div>
+    );
+}
---
+++ ReactProjects/Entry/test-app5/src/JokeData.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        testProp1: "testProp1 lorem ipsum...",
+        testProp2: "testProp2 lorem ipsum...",
+    },
+    {
+        testProp1: "testProp1 lorem ipsum...",
+        testProp2: "testProp2 lorem ipsum...",
+    },
+    {
+        testProp1: "testProp1 lorem ipsum...",
+        testProp2: "testProp2 lorem ipsum...",
+    },
+    {
+        testProp1: "testProp1 lorem ipsum...",
+        testProp2: "testProp2 lorem ipsum...",
+    },
+    {
+        testProp1: "testProp1 lorem ipsum...",
+        testProp2: "testProp2 lorem ipsum...",
+    }
+]
