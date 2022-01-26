cdc580c7e73a98d2f605137d0d56d0a070162ebc
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-26 18:19:24 +0300

    inputfield component updated // app.tsx states added

--- ReactProjects/Entry/test-app5/src/App.tsx
+++ ReactProjects/Entry/test-app5/src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 
 
 const App = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  console.log(todo);
+
   return (
     <div className="App">
       <span className='header'> Tasks List</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
--- ReactProjects/Entry/test-app5/src/components/InputField.tsx
+++ ReactProjects/Entry/test-app5/src/components/InputField.tsx
@@ -1,11 +1,23 @@
 import React from 'react'; //rafce
 import "./InputField.css";
 
-const InputField = () => {
+
+interface todoProps {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<todoProps> = ({ todo, setTodo }) => {
     return (
         <div>
-            <form action="" className='input'>
-                <input type="text" className='input__box' placeholder='Enter a Task' />
+            <form className='input'>
+                <input
+                    type="text"
+                    className='input__box'
+                    placeholder='Enter a Task'
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                />
                 <button type='submit' className='input__submit'> Submit</button>
             </form>
 
